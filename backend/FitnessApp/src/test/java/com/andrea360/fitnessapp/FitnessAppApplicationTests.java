ab293aa430a23d0f4793f29d195991b3f2286300
Vule <vukasinpatkovic@gmail.com>
2026-01-03 17:25:01 +0100

    chore: initial project structure and infrastructure setup

---
+++ backend/FitnessApp/src/test/java/com/andrea360/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrea360.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
