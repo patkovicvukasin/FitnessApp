84839639a7f0adf2e81fda612b7301cc3caa92ed
Vule <vukasinpatkovic@gmail.com>
2026-01-11 00:33:05 +0100

    fix(cors): update CORS allowed orgin

--- backend/FitnessApp/src/main/java/com/andrea360/fitnessapp/configuration/SecurityConfig.java
+++ backend/FitnessApp/src/main/java/com/andrea360/fitnessapp/configuration/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
